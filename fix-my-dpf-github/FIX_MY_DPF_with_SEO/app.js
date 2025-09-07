b9c8b6dcbbcd04a2460b05cce894c4050a1456a6
Zains95 <Z95@HOTMAIL.CO.UK>
2025-09-07 20:10:24 +0100

    Delete fix-my-dpf-github directory

--- fix-my-dpf-github/FIX_MY_DPF_with_SEO/app.js
+++
@@ -1,9 +0,0 @@
-
-const t = document.querySelector('.nav-toggle');
-const m = document.getElementById('menu');
-if (t && m){
-  t.addEventListener('click', () => {
-    const open = m.classList.toggle('open');
-    t.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-}
